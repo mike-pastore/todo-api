e97849568bfbfa7f6983255e2ec09a9b63f1c837
Mike Pastore <mike.t.pastore@gmail.com>
2015-12-13 15:57:19 -0600

    Install sequelizer, sqlite3

---
+++ playground/variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: 'Mike',
+// 	age: 21
+// };
+
+// function updatePerson(obj) {
+// 	// obj = {
+// 	// 	name: 'Mike',
+// 	// 	age: 24
+// 	// };
+
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Examples
+var grades = [15, 37];
+
+function addGrade(array, newGrade) {
+	array.push(newGrade);
+	debugger;
+}
+
+addGrade(grades, 50);
+console.log(grades);
