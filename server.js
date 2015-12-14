46e48418262021dfa42d3d81439d8306fff876c4
Mike Pastore <mike.t.pastore@gmail.com>
2015-12-13 19:54:03 -0600

    Add todo model and POST /todo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -55,16 +56,28 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	// db.todo.create
+	// if successful, respond with 200 and todo json
+	// if fails, e res.status(400).json(e)
 
-	body.description = body.description.trim();
-	body.id = todoNextId++;
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
 
-	todos.push(body);
 
-	res.json(body);
+
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
+
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
+
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -113,7 +126,8 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT);
-});
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT);
+	});
+})
