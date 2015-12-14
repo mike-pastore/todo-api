f073d7e64c5ebae24867c2dfaa1fd33a56fa9ac6
Mike Pastore <mike.t.pastore@gmail.com>
2015-12-13 22:59:46 -0600

    Add user model and POST /users request

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var db = require('./db.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
-var todoNextId = 1;
 
 app.use(bodyParser.json());
 
@@ -113,6 +112,17 @@ app.put('/todos/:id', function(req, res) {
 		}
 	}, function() {
 		res.status(500).send();
+	});
+});
+
+// POST /users
+app.post('/users', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(user) {
+		res.json(user.toJSON());
+	}, function(e) {
+		res.status(400).json(e);
 	});
 });
 
