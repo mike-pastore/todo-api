1b7164440ae0345c00816da2f329a63f9bca24d3
Mike Pastore <mike.t.pastore@gmail.com>
2015-12-14 17:03:20 -0600

    Add POST /user/login call

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require ('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -123,6 +124,31 @@ app.post('/users', function(req, res) {
 		res.json(user.toPublicJSON());
 	}, function(e) {
 		res.status(400).json(e);
+	});
+});
+
+// POST /users/login
+app.post('/users/login', function(req, res) {
+	var body = _.pick(req.body, 'email', 'password');
+
+	if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+		return res.status(400).send();
+	} 
+
+	// find matching email
+	db.user.findOne({
+		where: {
+			email: body.email
+		}
+	}).then(function(user) {
+		// check for no email, or invalid password
+		if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+			return res.status(401).send();
+		} 
+	
+		res.json(user.toPublicJSON());
+	}, function(e) {
+		res.status(500).send();
 	});
 });
 
