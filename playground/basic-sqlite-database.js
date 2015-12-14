4f1848e5cee46645688543ac232f6245a8867d58
Mike Pastore <mike.t.pastore@gmail.com>
2015-12-13 19:16:18 -0600

    Search with sequelize

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -1,4 +1,4 @@
-var Sequelize = require ('sequelize');
+var Sequelize = require('sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
 	// tell which database you want to use
 	'dialect': 'sqlite',
@@ -8,21 +8,57 @@ var sequelize = new Sequelize(undefined, undefined, undefined, {
 
 var Todo = sequelize.define('todo', {
 	description: {
-		type: Sequelize.STRING
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: [1, 250]
+		}
 	},
 	completed: {
-		type: Sequelize.BOOLEAN
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
 	}
 });
 
-sequelize.sync({force: true}).then(function () {
+sequelize.sync({
+	// force: true
+}).then(function() {
 	console.log('Everything is synced');
 
-	Todo.create({
-		description: 'Walk my dog',
-		completed: false
-	}).then(function (todo){
-		console.log('Finished!');
-		console.log(todo); 
+	Todo.findById(2).then(function (todo) {
+		if (todo) {
+			console.log(todo.toJSON());
+		} else {
+			console.log('Could not find that ID');
+		}		
 	});
+
+	// Todo.create({
+	// 	description: 'Take out trash'
+	// }).then(function(todo) {
+	// 	return Todo.create({
+	// 		description: 'Clean office'
+	// 	});
+	// }).then(function() {
+	// 	// return Todo.findById(1)
+	// 	return Todo.findAll({
+	// 		where: {
+	// 			description: {
+	// 				$like: '%trash%'
+	// 			}
+	// 		}
+	// 	});
+	// }).then(function (todos) {
+	// 	if (todos) {
+	// 		todos.forEach(function (todo) {
+	// 			console.log(todo.toJSON());
+	// 		});
+			
+	// 	} else {
+	// 		console.log('no todo found!');
+	// 	}
+	// }).catch(function(e) {
+	// 	console.log(e);
+	// });
 });
