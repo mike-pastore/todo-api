611e15ad144ac93360cec409c9d7c21bf5359056
Mike Pastore <mike.t.pastore@gmail.com>
2015-12-15 22:48:15 -0600

    Add todo // user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -21,44 +21,41 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	// force: true
 }).then(function() {
 	console.log('Everything is synced');
 
-	Todo.findById(2).then(function (todo) {
-		if (todo) {
-			console.log(todo.toJSON());
-		} else {
-			console.log('Could not find that ID');
-		}		
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where: {
+				description: {
+					$like: '%make%'
+				}
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
 	});
 
-	// Todo.create({
-	// 	description: 'Take out trash'
-	// }).then(function(todo) {
-	// 	return Todo.create({
-	// 		description: 'Clean office'
-	// 	});
+	// User.create({
+	// 	email: 'mike@example.com'
 	// }).then(function() {
-	// 	// return Todo.findById(1)
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%trash%'
-	// 			}
-	// 		}
+	// 	return Todo.create({
+	// 		description: 'Make dinner with Kera'
 	// 	});
-	// }).then(function (todos) {
-	// 	if (todos) {
-	// 		todos.forEach(function (todo) {
-	// 			console.log(todo.toJSON());
-	// 		});
-			
-	// 	} else {
-	// 		console.log('no todo found!');
-	// 	}
-	// }).catch(function(e) {
-	// 	console.log(e);
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user) {
+	// 		user.addTodo(todo);
+	// 	});
 	// });
 });
